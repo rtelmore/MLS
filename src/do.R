355381eda65890682bc5ccbecd4d193ffcbb3ba7
Ryan Elmore <rtelmore@gmail.com>
2010-08-23 09:01:04 -0600

    adding some stuff to do.R

--- src/do.R
+++ src/do.R
@@ -23,7 +23,7 @@ ggplot(data = mls.reg.dat, aes(x=year, y=team, fill=goals/games)) +
   geom_tile() +
   scale_fill_continuous(name="goals/game")
 
-ggsave("../figures/gpg_mls_2.png",hei=7,wid=7)
+ggsave("../figures/gpg_mls_2.png", hei = 7, wid = 7)
  
 ## Edit the data
 mls.reg.dat$h_tot <- as.numeric(gsub(",", "", mls.reg.dat$h_tot))
@@ -33,23 +33,37 @@ mls.reg.dat$a_avg <- as.numeric(gsub(",", "", mls.reg.dat$a_avg))
 
 
 
-## Shots on goal vs fs by team and by year
-ggplot(data = mls.reg.dat, aes(x = a_tot, y = sog)) + geom_point() + 
-  facet_wrap(~ team)
- 
-ggplot(data = mls.reg.dat, aes(x = fs, y = fc, col = team)) + 
-  geom_point() + facet_wrap(~ year)
-  
-  fill = )) + 
-  geom_tile() + scale_fill_continuous(name = "away")
+## Trying to find some interesting stuff...
 
-ggplot(data = mls.reg.dat, aes(x = year, y = shots)) + 
-  geom_point(col="navy") + geom_smooth()
-  
+mls.reg.dat$prior.2003 <- mls.reg.dat$year <= 2002
 
-ggplot(data = mls.reg.dat, aes(x = year, y = sog)) + 
-  geom_point(col="navy")
-  
 ggplot(data = mls.reg.dat, aes(x = year, y = sog/shots)) + 
-  geom_point(col="navy")
+  geom_point(col="navy") + geom_smooth(method = "lm") +
+  scale_y_continuous("shots on goal per shot")
 
+ggplot(data = mls.reg.dat, aes(x = year, y = sog/goals)) + 
+  geom_point(col="navy") + geom_smooth() +
+  scale_y_continuous("shots on goal per goal")
+
+ggplot(data = mls.reg.dat, aes(x = year, y = assists/goals)) + 
+  geom_point() + geom_smooth()
+
+ggplot(data = mls.reg.dat, aes(x = year, y = assists/goals, col = prior.2003)) +
+  scale_y_continuous(name = "assists per goal") + 
+  geom_point() + 
+  geom_smooth(method = "lm") 
+
+last_plot() + scale_colour_discrete("", breaks = c(TRUE, FALSE), 
+  labels = c("< 2003", ">= 2003"))
+
+ggsave("../figures/assists_per_goal.png", hei = 7, wid = 7)
+
+ggplot(data = mls.reg.dat, aes(x = year, y = assists)) +
+  geom_point() + geom_smooth() 
+
+ggsave("../figures/assists.png", hei = 7, wid = 7)
+
+ggplot(data = mls.reg.dat, aes(x = year, y = goals, col = prior.2003)) +
+  geom_point() + geom_smooth(metho = "lm") 
+
+ggsave("../figures/goals.png", hei = 7, wid = 7)
